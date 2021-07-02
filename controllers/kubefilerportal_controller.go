2cae6d2aed9cd3b2f5c8c769d7f248c817338dc2
Ygal Blum <ygal.blum@gmail.com>
2021-07-02 16:26:57 -0400

    KubeFilerPortal does not require Reconcile

--- controllers/kubefilerportal_controller.go
+++ controllers/kubefilerportal_controller.go
@@ -37,15 +37,7 @@ type KubeFilerPortalReconciler struct {
 //+kubebuilder:rbac:groups=kubefiler.ctera.com,resources=kubefilerportals/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=kubefiler.ctera.com,resources=kubefilerportals/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the KubeFilerPortal object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
+// The KubeFilerPortal resource is only configuration. Currently, updating it does not have any effect
 func (r *KubeFilerPortalReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
